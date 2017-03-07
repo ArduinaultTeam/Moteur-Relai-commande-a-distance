aeee7debda4df7821c303b28be9d8f403af0d6b3
Arhimeas <arhimeas@gmail.com>
2017-03-07 14:36:04 +0100

    Add files via upload

---
+++ nRF24l01/nrf24_emetteur_test.ino
@@ -0,0 +1,55 @@
+/*******************************************************************
+ Chaque seconde, un nombre est émis par une carte Arduino munie
+ d'un module nRF24L01.
+ Ce message peut être capté par un autre Arduino, ou par un Raspberry Pi.
+ http://electroniqueamateur.blogspot.ca/2017/02/communication-par-nrf24l01-entre-deux.html
+********************************************************************/
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+RF24 radio(7, 8);
+const uint64_t addresse = 0x1111111111;
+const int taille = 32;
+char message[taille + 1];
+
+char mot1[] = "Hello";
+char mot2[] = "World !";
+
+void createMessage(char valeur1[], char valeur2[])
+{
+  int size1 = sizeof(mot1);
+  Serial.println(size1);
+  
+  for(int i = 0; i < size1-1 ; i++)
+  {
+    message[i] = valeur1[i];
+  }
+  
+  int size2 = sizeof(mot2);
+  Serial.println(size2);
+  
+  for(int j = 0; j <= size2; j++)
+  {
+    message[j+size1-1] = valeur2[j];
+  }  
+
+  Serial.println(message);
+}
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println("Emetteur de donnees");
+  radio.begin();
+  radio.openWritingPipe(addresse);
+}
+
+void loop(void)
+{
+  createMessage(mot1,mot2);
+  radio.write( message, taille );
+  delay(1000);
+}
+
---
+++ nRF24l01/nrf24_recepteur_test.ino
@@ -0,0 +1,35 @@
+/***********************************************************
+Sketch permettant à l'Arduino de recevoir des messages
+en provenance d'un autre Arduino, ou d'un Raspberry, par 
+l'entremise d'un module nRF24L01.
+Les messages reçus sont affichés dans le moniteur série.
+http://electroniqueamateur.blogspot.ca/2017/02/communication-par-nrf24l01-entre-deux.html
+*************************************************************/
+
+#include <SPI.h>
+#include "nRF24L01.h"
+#include "RF24.h"
+
+RF24 radio(7, 8);
+const uint64_t adresse = 0x1111111111;
+const int taille = 32;
+char message[taille + 1]; 
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println("Recepteur RF24");
+  radio.begin();
+  radio.openReadingPipe(1, adresse);
+  radio.startListening();
+}
+
+void loop(void)
+{
+  while ( radio.available() )
+  {
+    radio.read( message, taille );
+    Serial.print("Message recu : ");
+    Serial.println(message);
+  }
+}
