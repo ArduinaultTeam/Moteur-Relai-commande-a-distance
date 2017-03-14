1d96e3f4b14a2dcbbfac429c7e39016c78498b42
Arhimeas <arhimeas@gmail.com>
2017-03-14 14:38:55 +0100

    Update attente.ino

--- Feux_tricolores/Etape2/attente.ino
+++ Feux_tricolores/Etape2/attente.ino
@@ -3,7 +3,7 @@ void attente()
   if(old_statut != statut)
     delay_start = millis();
 
-  while(millis() - delay_start > 3000)
+  while(millis() - delay_start < 3000)
   {
     read_buttons();
     update_tab();
